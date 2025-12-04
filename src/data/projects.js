c73aa2cd846b198215983106d1f87b5fbac052ab
Beatrice Errico <1.beatriceerrico@gmail.com>
2025-12-04 12:00:36 +0100

    save

--- src/data/projects.js
+++ src/data/projects.js
@@ -33,7 +33,7 @@ export const projects = [
       "Pagine di dettaglio con informazioni sul singolo gioco.",
       "Componenti riutilizzabili e gestione stato con hook React.",
     ],
-    liveUrl: "https://game-zone-errico.vercel.app/", 
+    liveUrl:"https://game-zone-errico.vercel.app/",
     codeUrl: "https://github.com/Beatrice-Errico/GameZone",
   },
   {
